1c6b7428fa86f65d39b566ab7903cab8c5791db5
A Stone <astone@heckgrammar.co.uk>
2023-01-03 10:44:39 +0000

    Add project files.

---
+++ UsingDictionary/Module1.vb
@@ -0,0 +1,14 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim dict As New Dictionary(Of Integer, String)
+        'Return the length Of the dictionary
+        'Add a New key: value pair to the dictionary
+        'Delete a key: value pair from the dictionary
+        'Amend the value in a key: value pair
+        'Return the value associated With key k
+        'Return True Or False depending On whether the key Is In the dictionary
+
+    End Sub
+
+End Module
